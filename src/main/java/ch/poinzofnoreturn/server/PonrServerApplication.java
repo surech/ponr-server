b60541e41eeed6c8e3d341efa1c7953ce37c88ec
Stefan Urech <stefan@surech.ch>
2015-11-14 16:47:00 +0100

    stu: Spring-Boot-Skeleton von Initializr übernommen

---
+++ src/main/java/ch/poinzofnoreturn/server/PonrServerApplication.java
@@ -0,0 +1,12 @@
+package ch.poinzofnoreturn.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonrServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PonrServerApplication.class, args);
+    }
+}
---
+++ src/test/java/ch/poinzofnoreturn/server/PonrServerApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.poinzofnoreturn.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PonrServerApplication.class)
+public class PonrServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
