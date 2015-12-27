fc7a964ea3452746511421efe4b19b93dfa9f6e7
Stefan Urech <stefan@surech.ch>
2015-12-27 16:28:02 +0100

    stu: Lade nur Anbieter mit Codes

--- src/main/java/ch/poinzofnoreturn/server/repo/ProviderRepository.java
+++ src/main/java/ch/poinzofnoreturn/server/repo/ProviderRepository.java
@@ -2,13 +2,22 @@ package ch.poinzofnoreturn.server.repo;
 
 import ch.poinzofnoreturn.server.model.ProviderEntity;
 import ch.poinzofnoreturn.server.projection.ProviderWithCode;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 /**
  * Datenbank-Zugriff auf die Anbieter
  */
 @RepositoryRestResource(collectionResourceRel = "provider", path="provider", excerptProjection = ProviderWithCode.class)
-public interface ProviderRepository extends CrudRepository<ProviderEntity, Long> {
+public interface ProviderRepository extends PagingAndSortingRepository<ProviderEntity, Long> {
 
+    @Query("SELECT p FROM ProviderEntity p JOIN p.pointcodes c")
+    @RestResource(path = "onlyWithCodes", rel = "onlyWithCodes")
+    public List<ProviderEntity> findProviderWithPointcodes(Pageable p);
 }
