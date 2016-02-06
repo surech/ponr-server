f7892263020529b5e52913325e6cec50d74e7552
Stefan Urech <stefan@surech.ch>
2016-02-06 17:49:33 +0100

    stu: Grundlegende Implementierung für OAuth 2.0-Autorisierung via Facebook. Nach dem Login wird man noch auf die falsche Seite weitergeleitet

--- src/main/java/ch/poinzofnoreturn/server/PonrServerApplication.java
+++ src/main/java/ch/poinzofnoreturn/server/PonrServerApplication.java
@@ -1,19 +1,84 @@
 package ch.poinzofnoreturn.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.filter.RequestContextFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EntityScan("ch.poinzofnoreturn.server.model")
 @EnableJpaRepositories("ch.poinzofnoreturn.server.repo")
 @EnableWebMvc
-public class PonrServerApplication {
+@EnableOAuth2Sso
+public class PonrServerApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(PonrServerApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/user")
+                    .authenticated()
+                .antMatchers("/**")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+        .and().logout().logoutSuccessUrl("/").permitAll()
+        .and().formLogin().defaultSuccessUrl("http://localhost:3000", true);
+
+//        http
+//                .antMatcher("/**")
+//                .authorizeRequests()
+//                    .antMatchers("/", "/login**", "/webjars/**")
+//                    .permitAll()
+//                .anyRequest()
+//                    .authenticated();
+    }
+
+    @Bean public RequestContextListener requestContextListener(){
+        return new RequestContextListener();
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        // return new CorsFilter(source);
+        final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
